5c71fbd3ddfa578e590b73c07fd12455e850c426
Greg Miller <hypurban86@gmail.com>
2025-07-11 09:00:52 -0400

    Adds version number to status bar so it's not empty

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -5,11 +5,36 @@
 	Bar with status infos along the bottom of the screen.
 -->
 <template>
-	<div>Status</div>
+	
+	<div class="statusBar">
+		<div class="version-number">v1.0.0</div>
+	</div>
 </template>
 <script setup>
 
 </script>
 <style lang="scss" scoped>
 
+	.statusBar {
+
+		// fill parent element
+		position: absolute;
+		inset: 0px 0px 0px 0px;
+
+		// for debug
+		/* border: 1px solid red; */
+
+		// fixed on bottom right
+		.version-number {
+
+			position: absolute;
+			top: 8px;
+			right: 10px;
+			color:rgba(239, 239, 239, 0.36);
+			font-size: 12px;
+			font-family: 'Courier New', Courier, monospace;
+
+		}// .version-number
+
+	}// .statusBar
 </style>
